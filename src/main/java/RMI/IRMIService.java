09760919aa5d76cff0b5b2bc1e907887ba2f8875
Jhon Jairo Corzo Calderon <jhon.corzo.2020@upb.edu.co>
2023-10-16 17:36:26 -0500

    RMI service interface

---
+++ src/main/java/RMI/IRMIService.java
@@ -0,0 +1,14 @@
+package RMI;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IRMIService extends Remote {
+    public String uploadFile(String nameWithExt, String userPath, byte[] fileData) throws RemoteException;
+    public byte[] downloadFile(String path) throws RemoteException;
+    public boolean createDirectory(String path) throws RemoteException;
+    public boolean removeFile(String path) throws RemoteException;
+    public boolean removeFolder(String path) throws RemoteException;
+    public boolean changeFilePath(String path, String newName) throws RemoteException;
+}
+
