946b407373f048062609ddc3c77157f9d78e34ac
Jhon Jairo Corzo Calderon <jhon.corzo.2020@upb.edu.co>
2023-10-17 00:07:27 -0500

    node controller guide

--- src/main/java/controller/NodeController.java
+++ src/main/java/controller/NodeController.java
@@ -11,12 +11,41 @@ import java.util.Queue;
 public class NodeController {
 
     private List<Node> availabeNodes = new ArrayList<Node>();
+    private Node node1 = new Node(1,"127.0.0.1", 1111, 100);
+    private Node node2 = new Node(1,"127.0.0.1", 1111, 100);
+    private Node node3 = new Node(1,"127.0.0.1", 1111, 100);
     private Queue<NodeRequest> requests = new LinkedList<>();
 
     public NodeController(){
-        availabeNodes.add(new Node(1,"127.0.0.1", 1111, 100));
-        availabeNodes.add(new Node(2,"127.0.0.2", 1111, 100));
-        availabeNodes.add(new Node(3,"127.0.0.3", 1111, 100));
+        availabeNodes.add(node1);
+        availabeNodes.add(node2);
+        availabeNodes.add(node3);
     }
 
+    // TODO: When a node ends a request it must be added to the list again
+    // TODO: Redefine the methods (void) and think about the parameters
+
+    public void uploadFile(){
+
+    }
+
+    public void downloadFile(){
+
+    }
+
+    public void updateFilePath(){
+
+    }
+
+    public void deleteFile(){
+
+    }
+
+    public void createFolder(){
+
+    }
+
+    public void deleteFolder(){
+
+    }
 }
