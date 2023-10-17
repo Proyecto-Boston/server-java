7460e34964a1a8a229f6228845de3cc582af12f7
Jhon Jairo Corzo Calderon <jhon.corzo.2020@upb.edu.co>
2023-10-16 23:43:57 -0500

    Node class

---
+++ src/main/java/node/Node.java
@@ -0,0 +1,32 @@
+package node;
+
+public class Node {
+    private int id;
+    private String ip;
+    private int port;
+    private int storageCapacity; // In gigabytes
+
+    public Node(int id, String ip, int port, int storageCapacity){
+        this.id = id;
+        this.ip = ip;
+        this.port = port;
+        this.storageCapacity = storageCapacity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public int getStorageCapacity() {
+        return storageCapacity;
+    }
+
+}
