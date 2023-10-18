252251d47b92bf9d17c1336abafd7a3a13295fa1
Jhon Jairo Corzo Calderon <jhon.corzo.2020@upb.edu.co>
2023-10-18 07:34:37 -0500

    :rocket:

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -12,13 +12,13 @@ public class Server {
 
         Service service = new Service();
         User usuario = new User();
-        usuario.email = "ejempl3@gmail.com";
+        usuario.email = "ejemplo2@gmail.com";
         usuario.password = "123456789";
         usuario.name = "Ejemplo";
         usuario.surname = "DePrueba";
 
         service.verifySession("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF91c2VyIjoxfQ.Q17XbTiobe4HscYgctzyM3zKCq-ZkjAgQrsr4C4uyJQ");
         File file = new File();
-        service.register(usuario);
+        //service.register(usuario);
     }
 }
